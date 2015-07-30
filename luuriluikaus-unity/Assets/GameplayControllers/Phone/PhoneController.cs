a41a503195cfabc942c3439b97adfa82b14e1588
Janne Koski <janne.koski@moziz.fi>
2015-07-30 21:45:23 +0300

    Add stub for phone controller

---
+++ luuriluikaus-unity/Assets/GameplayControllers/Phone/PhoneController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PhoneController : MonoBehaviour {
+    public Transform rotaryTrans;
+
+    public int rotaryDivision = 10;
+
+    bool rotating = false;
+    float rotatyValue = 0f;
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            // Start rotating
+
+        } else if (Input.GetMouseButtonUp(0)) {
+            // Stop rotating
+
+        }
+    }
+}
