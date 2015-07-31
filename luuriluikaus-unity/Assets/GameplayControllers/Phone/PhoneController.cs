fa7cc113164e719632fe2231a8790763a40ae5e5
Janne Koski <janne.koski@moziz.fi>
2015-07-31 21:10:43 +0300

    Do the phone!

--- luuriluikaus-unity/Assets/GameplayControllers/Phone/PhoneController.cs
+++ luuriluikaus-unity/Assets/GameplayControllers/Phone/PhoneController.cs
@@ -20,18 +20,29 @@ public class PhoneController : MonoBehaviour {
 
     // Settings 
     public int rotaryDivision = 9;
-    public double rollbackSpeed = Mathf.PI / 16f;
+    public float rotaryRangeNumbersBegin = 0.05f;
+    public float rotaryRangeNumbersEnd = 0.6f;
+    public float rotaryRangeBegin = 0.1f;
+    public float rotaryRangeEnd = 0.95f;
+    public float rollbackSpeed = Mathf.PI / 32f;
+
+    // State
+    public RotaryState state {
+        get;
+        private set;
+    }
 
     Collider rotaryCollider;
     Transform rotaryTrans;
 
     Quaternion originalOrientation;
 
-    bool isRotating = false;
-    float currentRotaryRotation = 0f;
+    float rotateStart = 0f;
+    float rotateReleaseTime = 0f;
+    float rotaryReleaseDelta = 0f;
+    float rotaryDelta = 0f;
 
     Vector2 inputCenterPoint;
-    Vector2 inputPosLastFrame;
 
     OnRotaryStart event_rotaryStart;
     OnRotaryRelease event_rotaryRelease;
@@ -54,6 +65,8 @@ public class PhoneController : MonoBehaviour {
         rotaryTrans = rotaryObject.GetComponent<Transform>();
 
         originalOrientation = rotaryTrans.rotation;
+
+        state = RotaryState.rollback;
     }
 
     void Start() {
@@ -62,69 +75,87 @@ public class PhoneController : MonoBehaviour {
 
     void Update() {
         inputCenterPoint = viewportCamera.WorldToScreenPoint(rotaryTrans.position);
-        Vector2 clickPos = Input.mousePosition;
+        Vector2 inputPos = Input.mousePosition;
+        float currentInputAngle = CalculateRotationAtPoint(inputPos);
 
-        if (Input.GetMouseButtonDown(0) && !isRotating) {
+        if (Input.GetMouseButtonDown(0) && state == RotaryState.rest) {
             // Start new rotation if hits
 
-            Ray clickRay = viewportCamera.ScreenPointToRay(clickPos);
-
-            // Check hit
+            Ray clickRay = viewportCamera.ScreenPointToRay(inputPos);
             RaycastHit rayHit;
             if (rotaryCollider.Raycast(clickRay, out rayHit, 10000f)) {
-                // start rotating
-
-                isRotating = true;
-                inputPosLastFrame = clickPos;
-
+                // Start selecting
+                rotateStart = currentInputAngle;
+                state = RotaryState.selecting;
+                
                 if (event_rotaryStart != null) {
                     event_rotaryStart();
                 }
-
-            } else {
-                // Did not hit. Ignore
             }
+        }
 
-        } else if (Input.GetMouseButtonUp(0) && isRotating) {
-            // Stop rotating
+        if (state == RotaryState.selecting) {
+            float newDelta = currentInputAngle - rotateStart;
 
-            float rotaryValueF = ((currentRotaryRotation / Mathf.PI) + 1f) / 2f;
-            int rotaryOutputValue = ((int)(rotaryValueF * rotaryDivision) + 1);
+            if (newDelta > rotaryDelta) {
+                rotaryDelta = currentInputAngle - rotateStart;
+
+                // Prevent going over end
+                if (rotaryDelta + rotateStart > rotaryRangeEnd) {
+                    rotaryDelta = rotaryRangeEnd - rotateStart;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && state == RotaryState.selecting) {
+            // Stop selecting
+
+            // Check if the rotation is inside the number selection range
+            
+            rotaryReleaseDelta = rotaryDelta;
+            rotateReleaseTime = Time.time;
+            state = RotaryState.rollback;
+
+            int rotaryOutputValue = -1;
+            float numberRangeNormalized = (rotaryDelta - rotaryRangeNumbersBegin) / (rotaryRangeNumbersEnd - rotaryRangeNumbersBegin);
+            rotaryOutputValue = (int)(numberRangeNormalized * rotaryDivision) + 1;
 
             if (rotaryOutputValue == rotaryDivision) {
                 rotaryOutputValue = 0;
             }
 
-            Debug.Log("Rotaty released. Output value: " + rotaryOutputValue);
-
-            isRotating = false;
-
+            Debug.Log("Rotaty released. releaseDelta: " + rotaryReleaseDelta + " norm: " + numberRangeNormalized + " Output value: " + rotaryOutputValue);
             if (event_rotaryRelease != null) {
                 event_rotaryRelease(rotaryOutputValue);
             }
-
-            // TODO Do this after rollback
-            if (event_rotaryEnd != null) {
-                event_rotaryEnd();
-            }
-
-        } else if (Input.GetMouseButton(0) && isRotating) {
-            // Update rotation
-            Vector2 tanttarelli = clickPos - inputCenterPoint;
-            currentRotaryRotation = Mathf.Atan2(tanttarelli.x, tanttarelli.y);
-            rotaryTrans.rotation = Quaternion.AngleAxis((currentRotaryRotation / (2f * Mathf.PI)) * 360f, originalOrientation * Vector3.up) * originalOrientation;
-
-            inputPosLastFrame = clickPos;
         }
         
-        if (!isRotating) {
-            // Rollback
+        if (state == RotaryState.rollback) {
+            if (rotaryDelta > 0f) {
+                rotaryDelta = (Time.time - rotateReleaseTime) * (-rollbackSpeed) + rotaryReleaseDelta;
 
-            // TODO Do this after rollback
-            //if (event_rotaryEnd != null) {
-            //  event_rotaryEnd();
-            //}
+            } else {
+                rotaryDelta = 0f;
+
+                state = RotaryState.rest;
+
+                if (event_rotaryEnd != null) {
+                    event_rotaryEnd();
+                }
+            }
         }
+
+        // Update visualization
+        if (state == RotaryState.selecting || state == RotaryState.rollback) {
+            rotaryTrans.rotation = Quaternion.AngleAxis((rotaryDelta + rotaryRangeBegin) * 360f, originalOrientation * Vector3.up) * originalOrientation;
+        }
+    }
+
+    float CalculateRotationAtPoint(Vector2 point) {
+        Vector2 tanttarelli = point - inputCenterPoint;
+        float angle = ((Mathf.Atan2(tanttarelli.x, tanttarelli.y) / Mathf.PI) + 1f) / 2f;
+
+        return angle;
     }
 
     public void SubscribeOnRotaryRelease(OnRotaryRelease listener) {
