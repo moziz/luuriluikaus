7f517770e34079eb8f782f80fd3d9c297d1c318b
Janne Koski <janne.koski@moziz.fi>
2015-08-01 22:23:55 +0300

    Improve TV glow blinking effect

--- luuriluikaus-unity/Assets/glowScript.cs
+++ luuriluikaus-unity/Assets/glowScript.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 
 public class glowScript : MonoBehaviour {
-	
+    public float blinkSpeed = 16f;
+    
 	Light myLight;
-
+    float originalIntensity;
+    float intensityDeltaMultiplier = 0.3f;
+    
 	// Use this for initialization
 	void Start () {
 		myLight = GetComponent<Light>();
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		myLight.intensity = Mathf.PingPong(Time.time*0.2f, 0.5f) + 0.5f;
-	
+        originalIntensity = myLight.intensity;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		myLight.intensity = originalIntensity - Mathf.PingPong(blinkSpeed * Time.time + Random.value, originalIntensity * intensityDeltaMultiplier);
 	}
 }
