71fafb3d1b4d4c3dd19439f8b8719ce20e2589e4
Raattis <raattis@gmail.com>
2015-08-02 03:53:23 +0300

    Newspaper transition

---
+++ luuriluikaus-unity/Assets/TravelBetweenTwoTransforms.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TravelBetweenTwoTransforms : MonoBehaviour
+{
+    public Transform start;
+    public Transform mid;
+    public Transform end;
+    public Camera cam;
+
+    public float current = 0;
+    public float target = 1;
+    float speed = 1;
+    bool visible = true;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            foreach (RaycastHit hit in Physics.RaycastAll(cam.ScreenPointToRay(Input.mousePosition)))
+            {
+                if(hit.collider.transform == transform)
+                {
+                    visible = !visible;
+                    if (visible)
+                    {
+                        target = 1;
+                    }
+                    else
+                    {
+                        target = 0;
+                    }
+                    break;
+                }
+            }
+        }
+
+        if (current != target)
+        {
+            if(target > current)
+            {
+                current += Time.deltaTime * speed;
+                if(target < current)
+                {
+                    current = target;
+                }
+            }
+            else
+            {
+                current -= Time.deltaTime * speed;
+                if (target > current)
+                {
+                    current = target;
+                }
+            }
+
+            transform.position = Vector3.Lerp(start.position, end.position, current);
+            transform.position = Vector3.Lerp(transform.position, mid.position, Mathf.Sin(current * Mathf.PI) * 0.5f);
+            transform.rotation = Quaternion.Lerp(start.rotation, end.rotation, current);
+        }
+    }
+}
